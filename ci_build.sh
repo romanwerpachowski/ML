0e6e53d5c18d914e0c501437d537b6549aa0d178
Roman Werpachowski <roman.werpachowski@gmail.com>
2020-08-31 13:44:28 +0100

    Add Demo

--- ci_build.sh
+++ ci_build.sh
@@ -2,12 +2,17 @@
 
 set -ev
 
-# Build.
+#*** Debug build ***
 scons -j 2
+
+#*** Release build ***
 scons mode=release -j 2
 
-# Test.
+#*** Unit tests ***
 ./Tests/build/Debug/Tests
 
-# Benchmark.
+#*** Demos ***
+./Demo/build/Release/Demo
+
+#*** Benchmarks ***
 ./Benchmarks/build/Release/Benchmarks
