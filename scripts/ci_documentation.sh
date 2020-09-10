e72e0bbdaa05f3486b916d4d80343ad3f6259edb
Roman Werpachowski <roman.werpachowski@gmail.com>
2020-09-11 00:56:36 +0100

    Fix problem with serving _static and _sources.

--- scripts/ci_documentation.sh
+++ scripts/ci_documentation.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -ev
 
+CWD=$(pwd)
+
 #*** Generate documentation for the C++ code using Doxygen. ***
 doxygen Doxyfile
 
 #*** Generate documentation for Python extension using Sphinx. ***
+cd "${CWD}"
 cd docs/sphinx
 make clean
 make html
 cd ..
 mv -f sphinx/_build cppyml
 rm -rf sphinx
-cd ..
+cd cppyml/html
+# Work around Jekyll's unwillingness to serve directories with names
+# beginning in underscores.
+mv _static static
+mv _sources sources
+sed -i -e 's/_static\//static\//g' *.html
+sed -i -e 's/_sources\//sources\//g' *.html
+# Go back to main directory.
+cd "${CWD}"
