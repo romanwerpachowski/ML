4b616f1adf4829a5f47c71e811729f33f9338d5d
Roman Werpachowski <roman.werpachowski@gmail.com>
2021-03-21 11:13:18 +0100

    Working k-NN search.

--- ML/BallTree.cpp
+++ ML/BallTree.cpp
@@ -1,5 +1,6 @@
 /** (C) 2021 Roman Werpachowski */
 #include <cassert>
+#include <iostream>
 #include <limits>
 #include "BallTree.hpp"
 
@@ -13,19 +14,43 @@ namespace ml
         if (X.cols()) {
             construct(work, 0, Features::from_vector(features));
         }
-    }    
+    }
+
+    unsigned int BallTree::left_child_index(unsigned int i) const
+    {
+        if (i >= size()) {
+            throw std::out_of_range("BallTree: node index out of range");
+        }
+        if (num_left_children_[i]) {
+            return i + 1;
+        } else {
+            return size();
+        }
+    }
+
+    unsigned int BallTree::right_child_index(unsigned int i) const
+    {
+        if (i >= size()) {
+            throw std::out_of_range("BallTree: node index out of range");
+        }
+        if (num_right_children_[i]) {
+            return i + 1 + num_left_children_[i];
+        } else {
+            return size();
+        }
+    }
 
     void BallTree::find_k_nearest_neighbours(Eigen::Ref<const Eigen::VectorXd> x, const unsigned int k, std::vector<unsigned int>& nn) const
     {
         if (x.size() != tree_.rows()) {
             throw std::invalid_argument("BallTree: wrong feature vector size");
         }
-        std::queue<unsigned int> q;
+        MaxDistancePriorityQueue q;
         knn_search(x, k, 0, q);
         nn.reserve(q.size());
         nn.clear();
         while (!q.empty()) {
-            nn.push_back(q.front());
+            nn.push_back(q.top().first);
             q.pop();
         }
     }
@@ -39,25 +64,26 @@ namespace ml
         if (static_cast<unsigned int>(nn.cols()) < num_neighbours) {
             throw std::invalid_argument("BallTree: not enough room for all neighbours");
         }
-        std::queue<unsigned int> q;
+        MaxDistancePriorityQueue q;
         knn_search(x, k, 0, q);
         assert(q.size() == static_cast<size_t>(num_neighbours));
         Eigen::Index i = 0;
         while (!q.empty()) {
-            nn.col(i) = tree_.col(q.front());
+            nn.col(i) = tree_.col(q.top().first);
             q.pop();
             ++i;
         }
         return num_neighbours;
     }
 
-    void BallTree::construct(Eigen::Ref<Eigen::MatrixXd> work, const Eigen::Index i, Features::VectorRange<Features::IndexedFeatureValue> features)
+    void BallTree::construct(Eigen::Ref<Eigen::MatrixXd> work, const unsigned int i, Features::VectorRange<Features::IndexedFeatureValue> features)
     {
         assert(i < tree_.cols());
         if (work.cols() == 0) {
             return;
         } else if (work.cols() == 1) {            
             tree_.col(i) = work.col(0);
+            radii_[i] = 0;
             num_left_children_[i] = 0;
             num_right_children_[i] = 0;
         } else {
@@ -94,55 +120,54 @@ namespace ml
             num_left_children_[i] = num_left;
             num_right_children_[i] = num_right;
             if (num_left) {
-                construct(work.block(0, 0, work.rows(), pivot_idx), i + 1, Features::VectorRange<Features::IndexedFeatureValue>(features.first, features.first + pivot_idx));
+                construct(work.block(0, 0, work.rows(), pivot_idx), left_child_index(i), Features::VectorRange<Features::IndexedFeatureValue>(features.first, features.first + pivot_idx));
             }
             if (num_right) {
-                construct(work.block(0, pivot_idx + 1, work.rows(), num_right), i + 1 + num_left, Features::VectorRange<Features::IndexedFeatureValue>(features.first + pivot_idx + 1, features.second));
+                construct(work.block(0, pivot_idx + 1, work.rows(), num_right), right_child_index(i), Features::VectorRange<Features::IndexedFeatureValue>(features.first + pivot_idx + 1, features.second));
             }
         }
     }
 
-    void BallTree::knn_search(Eigen::Ref<const Eigen::VectorXd> x, const unsigned int k, const Eigen::Index i, std::queue<unsigned int>& q) const
+    void BallTree::knn_search(Eigen::Ref<const Eigen::VectorXd> x, const unsigned int k, const unsigned int i, MaxDistancePriorityQueue& q) const
     {
-        assert(i < tree_.cols());
+        assert(i < size());
         const double dist_from_i = (x - tree_.col(i)).norm();
-        const double dist_from_q = distance_from_queue(x, q);
+        const double dist_from_q = distance_from_queue(x, k, q);
         if (dist_from_i - radii_[i] >= dist_from_q) {
             return;
         }
         if (dist_from_i < dist_from_q) {
-            q.push(static_cast<unsigned int>(i));
+            q.push(IndexedDistanceFromTarget(i, dist_from_i));
             if (q.size() > k) {
                 q.pop();
             }
-        }
-        Eigen::Index i1 = -1;
-        Eigen::Index i2 = -1;
+        }        
+        unsigned int i1 = left_child_index(i);
+        unsigned int i2 = right_child_index(i);
         double d1 = std::numeric_limits<double>::infinity();
         double d2 = std::numeric_limits<double>::infinity();
-        if (num_left_children_[i]) {
-            i1 = i + 1;
+        if (i1 < size()) {
             d1 = (x - tree_.col(i1)).norm();
         }
-        if (num_right_children_[i]) {
-            i2 = i + 1 + num_left_children_[i];
+        if (i2 < size()) {
             d2 = (x - tree_.col(i2)).norm();
         }
         if (d2 < d1) {
             std::swap(i1, i2);
         }
-        if (i1 >= 0) {
+        if (i1 < size()) {
             knn_search(x, k, i1, q);
         }
-        if (i2 >= 0) {
+        if (i2 < size()) {
             knn_search(x, k, i2, q);
         }
     }
 
-    double BallTree::distance_from_queue(Eigen::Ref<const Eigen::VectorXd> x, const std::queue<unsigned int>& q) const
+    double BallTree::distance_from_queue(Eigen::Ref<const Eigen::VectorXd> x, const unsigned int k, const MaxDistancePriorityQueue& q) const
     {
-        if (!q.empty()) {
-            return (x - tree_.col(q.front())).norm();
+        assert(q.size() <= k);
+        if (k && q.size() == k) {
+            return q.top().second;
         } else {
             return std::numeric_limits<double>::infinity();
         }
--- ML/BallTree.hpp
+++ ML/BallTree.hpp
@@ -1,6 +1,7 @@
 #pragma once
 /** (C) 2021 Roman Werpachowski */
 #include <queue>
+#include <utility>
 #include <Eigen/Core>
 #include "Features.hpp"
 #include "dll.hpp"
@@ -23,7 +24,7 @@ namespace ml
         DLL_DECLSPEC BallTree(Eigen::Ref<const Eigen::MatrixXd> X);
 
         /**
-         * @brief Returns const reference to feature vectors ordered as binary tree.
+         * @brief Returns const reference to feature vectors ordered as binary tree. The root is `#tree().col(0)`.
         */
         const Eigen::MatrixXd& tree() const
         {
@@ -31,10 +32,24 @@ namespace ml
         }
 
         /**
+         * @brief Returns the index of the left child of the i-th element in the tree (#tree().col(i)), or `#size()` if this node has no left children.
+         *
+         * @throw std::out_of_range If `i >= #size()`.
+        */
+        DLL_DECLSPEC unsigned int left_child_index(unsigned int i) const;
+
+        /**
+         * @brief Returns the index of the right child of the i-th element in the tree (#tree().col(i)), or `#size()` if this node no right children.
+         *
+         * @throw std::out_of_range If `i >= #size()`.
+        */
+        DLL_DECLSPEC unsigned int right_child_index(unsigned int i) const;
+
+        /**
          * @brief Finds up to k nearest neighbours for given target vector.
          * @param[in] x Target vector.
          * @param[in] k Number of nearest neighbours.
-         * @param[out] nn Upon return, has size `min(k, tree().cols())` and contains the indices of up to `k` nearest neighbours of `x`.
+         * @param[out] nn Upon return, has size `min(k, size())` and contains the indices of up to `k` nearest neighbours of `x`.
          * @throw std::invalid_argument If `x.size() != #tree().rows()`.
         */
         DLL_DECLSPEC void find_k_nearest_neighbours(Eigen::Ref<const Eigen::VectorXd> x, unsigned int k, std::vector<unsigned int>& nn) const;
@@ -47,17 +62,66 @@ namespace ml
          * @return Number of nearest neighbours found.
          * @throw std::invalid_argument If `x.size() != #tree().rows()`, `nn.rows() != #tree().rows()` or `nn.cols()` is lower than the number of nearest neighbours found.
         */
-        DLL_DECLSPEC unsigned int find_k_nearest_neighbours(Eigen::Ref<const Eigen::VectorXd> x, unsigned int k, Eigen::Ref<Eigen::MatrixXd> nn) const;
+        DLL_DECLSPEC unsigned int find_k_nearest_neighbours(Eigen::Ref<const Eigen::VectorXd> x, unsigned int k, Eigen::Ref<Eigen::MatrixXd> nn) const;        
+
+        /**
+         * @brief Size of the tree.
+        */
+        auto size() const
+        {
+            return static_cast<unsigned int>(tree_.cols());
+        }
     private:
         Eigen::MatrixXd tree_; /**< Ball tree data. */
         Eigen::VectorXd radii_; /**< radii_[i] is the radius of the subtree with root in tree_[i]. */
         std::vector<unsigned int> num_left_children_; /**< Number of left children for every node. */
         std::vector<unsigned int> num_right_children_; /**< Number of right children for every node. */
 
-        void construct(Eigen::Ref<Eigen::MatrixXd> work, Eigen::Index i, Features::VectorRange<Features::IndexedFeatureValue> features);
+        /**
+         * @brief Recursively constructs a ball tree.
+         * @param work Work matrix, containing features being processed.
+         * @param i Index of the current node in the tree.
+         * @param features Range of iterators to a work vector used for sorting feature values.
+        */
+        void construct(Eigen::Ref<Eigen::MatrixXd> work, unsigned int i, Features::VectorRange<Features::IndexedFeatureValue> features);
 
-        void knn_search(Eigen::Ref<const Eigen::VectorXd> x, unsigned int k, Eigen::Index i, std::queue<unsigned int>& q) const;
+        /**
+         * @brief Pair of (index of the feature vector in the tree, distance of the tree feature vector from the target vector).
+        */
+        typedef std::pair<unsigned int, double> IndexedDistanceFromTarget;
 
-        double distance_from_queue(Eigen::Ref<const Eigen::VectorXd> x, const std::queue<unsigned int>& q) const;
+        /**
+         * @brief Compares `IndexedDistanceFromTarget` objects so that furthest vectors come first in the queue.
+        */
+        struct IndexedDistanceFromTargetComparator
+        {
+            bool operator()(const IndexedDistanceFromTarget& a, const IndexedDistanceFromTarget& b) const
+            {
+                return a.second < b.second;
+            }
+        };
+
+        /**
+         * @brief Max-first priority queue used to store the current candidates for k nearest neighbours.
+        */
+        typedef std::priority_queue<IndexedDistanceFromTarget, std::vector<IndexedDistanceFromTarget>, IndexedDistanceFromTargetComparator> MaxDistancePriorityQueue;
+
+        /**
+         * @brief Recursively finds k nearest neighbours (or less if there's not enough data) of the target vector.
+         * @param x Target feature vector.
+         * @param k Max. number of neighbours to find.
+         * @param i Index of the current node in the tree where we search.
+         * @param q Max-first priority queue storing current NN candidates. Ordered by distance from `x`.
+        */
+        void knn_search(Eigen::Ref<const Eigen::VectorXd> x, unsigned int k, unsigned int i, MaxDistancePriorityQueue& q) const;
+
+        /**
+         * @brief Gets the max. distance of `x` from the candidates in the queue.
+         * @param x Target feature vector. 
+         * @param k Max. number of neighbours to find.
+         * @param q Max-first priority queue storing current NN candidates. Ordered by distance from `x`.
+         * @return Max. distance if `q.size() == k`, +Inf otherwise.
+        */
+        double distance_from_queue(Eigen::Ref<const Eigen::VectorXd> x, unsigned int k, const MaxDistancePriorityQueue& q) const;
     };
 }
