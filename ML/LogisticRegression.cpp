ad1fc27252d43bd8568892cf4bb759717d650b57
Roman Werpachowski <roman.werpachowski@gmail.com>
2021-11-04 00:29:49 +0100

    Basic functions for LR

--- ML/LogisticRegression.cpp
+++ ML/LogisticRegression.cpp
@@ -1,4 +1,5 @@
 /* (C) 2021 Roman Werpachowski. */
+#include <cassert>
 #include <cmath>
 #include <stdexcept>
 #include "LogisticRegression.hpp"
@@ -7,14 +8,46 @@ namespace ml
 {
     double LogisticRegression::probability(Eigen::Ref<const Eigen::VectorXd> x, double y, Eigen::Ref<const Eigen::VectorXd> w)
     {
+        assert(y == -1 || y == 1);
         return 1 / (1 + exp(-y * w.dot(x)));
     }
 
-    double LogisticRegression::likelihood(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y, Eigen::Ref<const Eigen::VectorXd> w, const double lam)
+    double LogisticRegression::log_likelihood(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y, Eigen::Ref<const Eigen::VectorXd> w, const double lam)
     {
         if (lam < 0) {
             throw std::domain_error("Lambda cannot be negative");
         }
-        // NFY
+        if (y.size() != X.cols()) {
+            throw std::invalid_argument("Size mismatch: y.size() != X.cols()");
+        }
+        if (w.size() != X.rows()) {
+            throw std::invalid_argument("Size mismatch: w.size() != X.rows()");
+        }
+        double l = 0;
+        for (Eigen::Index i = 0; i < y.size(); ++i) {
+            l -= log1p(exp(-y[i] * w.dot(X.col(i))));
+        }
+        l -= lam * w.squaredNorm() / 2;
+        return l;
+    }
+
+    void LogisticRegression::grad_log_likelihood(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y, Eigen::Ref<const Eigen::VectorXd> w, double lam, Eigen::Ref<Eigen::VectorXd> g)
+    {
+        if (lam < 0) {
+            throw std::domain_error("Lambda cannot be negative");
+        }
+        if (y.size() != X.cols()) {
+            throw std::invalid_argument("Size mismatch: y.size() != X.cols()");
+        }
+        if (w.size() != X.rows()) {
+            throw std::invalid_argument("Size mismatch: w.size() != X.rows()");
+        }
+        if (w.size() != g.size()) {
+            throw std::invalid_argument("Size mismatch: w.size() != g.size()");
+        }
+        g = - lam * w;
+        for (Eigen::Index i = 0; i < y.size(); ++i) {
+            g += probability(X.col(i), -y[i], w) * y[i] * X.col(i);
+        }
     }
 }
--- ML/LogisticRegression.hpp
+++ ML/LogisticRegression.hpp
@@ -6,11 +6,11 @@ namespace ml
 {
     /** @brief Binomial logistic regression algorithm.
     *
-    * Based on Thomas P. Minka, "A comparison of numerical optimizers for logistic regression", but modifying the labels to be 0 or 1, not +/-1.
+    * Based on Thomas P. Minka, "A comparison of numerical optimizers for logistic regression".
     * 
     * The model is:
     * 
-    * \f$ P(y = 0 or 1 | \vec{x}, \vec{w}) = \frac{1}{1 + \exp(-y \vec{w} \cdot \vec{x} )} \f$
+    * \f$ P(y = +/-1 | \vec{x}, \vec{w}) = \frac{1}{1 + \exp(-y \vec{w} \cdot \vec{x} )} \f$
     * 
     * We perform a MAP estimation of \f$ \vec{w} \f$, using a prior \f$ p(\vec{w}) \sim N(0, \lambda^{-1} I) \f$.
     * 
@@ -41,7 +41,7 @@ namespace ml
          * If fitting with intercept is desired, include a row of 1's in the X values.
          * 
          * @param X D x N matrix of X values, with data points in columns.
-         * @param y Y vector with length N. Values should be 0 or 1.
+         * @param y Y vector with length N. Values should be -1 or 1.
          * @return Result object.
         */
         virtual Result fit(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y) = 0;
@@ -50,20 +50,34 @@ namespace ml
         /**
          * @brief Calculates the probability of label given model weights and feature vector.
          * @param x Feature vector.
-         * @param y Label (0 or 1).
+         * @param y Label (-1 or 1).
          * @param w Model weight vector, equal length to `x`.
          * @return P(y|x,w).
         */
         static double probability(Eigen::Ref<const Eigen::VectorXd> x, double y, Eigen::Ref<const Eigen::VectorXd> w);
 
         /**
-         * @brief Calculates the data likelihood given model weights.
+         * @brief Calculates the posterior log-likelihood of data given model weights.
          * @param X D x N matrix of X values, with data points in columns.
-         * @param y Y vector with length N. Values should be 0 or 1.
+         * @param y Y vector with length N. Values should be -1 or 1.
          * @param w Model weight vector with length D.
-         * @param lam Regularisation parameter (inverse variance of the prior for `w`). Cannot be negative.
-         * @return Likelihood.
+         * @param lam Inverse variance of the Gaussian prior for `w`. Cannot be negative. Set it to 0 if you want to perform maximum likelihood estimation of `w`.
+         * @return Log-likelihood.
+         * @throw std::domain_error If `lam` is negative.
+         * @throw std::invalid_argument If matrix or vector dimensions do not match.
         */
-        static double likelihood(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y, Eigen::Ref<const Eigen::VectorXd> w, double lam);
+        static double log_likelihood(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y, Eigen::Ref<const Eigen::VectorXd> w, double lam);
+
+        /**
+         * @brief Calculates the gradient posterior log-likelihood of data given model weights, over those weights.
+         * @param X D x N matrix of X values, with data points in columns.
+         * @param y Y vector with length N. Values should be -1 or 1.
+         * @param w Model weight vector with length D.
+         * @param lam Inverse variance of the Gaussian prior for `w`. Cannot be negative. Set it to 0 if you want to perform maximum likelihood estimation of `w`.
+         * @param[out] g Vector with length D for the computed gradient of log-likelihood over weights `w`.
+         * @throw std::domain_error If `lam` is negative.
+         * @throw std::invalid_argument If matrix or vector dimensions do not match.
+        */
+        static void grad_log_likelihood(Eigen::Ref<const Eigen::MatrixXd> X, Eigen::Ref<const Eigen::VectorXd> y, Eigen::Ref<const Eigen::VectorXd> w, double lam, Eigen::Ref<Eigen::VectorXd> g);
     };
 }
