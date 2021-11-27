e0a54efd191c6540d819396ebdf0594184c13279
Roman Werpachowski <roman.werpachowski@gmail.com>
2021-11-28 00:36:26 +0100

    Fix documentation build.

--- scripts/ci_dependencies.sh
+++ scripts/ci_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-PACKAGES=$(cat ubuntu_required_packages.txt | tr -s '\n' ' ')
+PACKAGES=$(cat "$1" | tr -s '\n' ' ')
 
 sudo apt-get update
 sudo apt-get -y install ${PACKAGES}
