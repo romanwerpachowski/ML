b4c6705a8ee85f18150a0b0e985803423ebc432d
Roman Werpachowski <roman.werpachowski@gmail.com>
2021-11-29 23:02:05 +0100

    Keep the .so file.

--- scripts/ci_release_build.sh
+++ scripts/ci_release_build.sh
@@ -8,7 +8,7 @@ scons mode=release -j 2
 #*** Deploy Python module locally ***
 CWD="${PWD}"
 cd cppyml
-sudo python3 setup.py install bdist_wheel
+sudo python3 setup.py install
 cd ${CWD}
 
 #*** Python unit tests using the C++ release build ***
