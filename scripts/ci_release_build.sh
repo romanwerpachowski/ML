49b87d0becfecbbfdf008fe5a0a56964667954ed
Roman Werpachowski <roman.werpachowski@gmail.com>
2021-11-29 22:28:49 +0100

    Upload all cppyml/dist/ directory.

--- scripts/ci_release_build.sh
+++ scripts/ci_release_build.sh
@@ -9,8 +9,6 @@ scons mode=release -j 2
 CWD="${PWD}"
 cd cppyml
 sudo python3 setup.py install bdist_wheel
-# Copy the single generated wheel file to known name.
-cp dist/*.whl ${CWD}/cppyml.whl
 cd ${CWD}
 
 #*** Python unit tests using the C++ release build ***
